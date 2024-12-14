4c6977eb98f3a6132466c6f352f7425e3c349167
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-14 21:50:15 +0530

    fifth commit

---
+++ server/src/config/security.js
@@ -0,0 +1,15 @@
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const process = require("node:process");
+const cors = require("cors");
+
+module.exports = (app) => {
+  app.use(helmet());
+  app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // Limit each IP to 100 requests
+    })
+  );
+};
