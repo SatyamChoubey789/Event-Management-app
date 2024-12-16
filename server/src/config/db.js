d80ce28658a17731eb30fcb84c86e88dfe6430b6
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-17 03:24:03 +0530

    15 commit

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const {DB_NAME} = require("../constrants");
 const process = require("node:process");
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DB_NAME}`
+      `${process.env.MONGO_URI}`
     );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
