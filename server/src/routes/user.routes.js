f7b65868326e1852391051f57791690a322b4ae1
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-18 00:40:46 +0530

    16 commit

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 const userRoutes = new Router();
+const {
+  getCurrentUser,
+  updateAccountDetails,
+} = require("../controllers/user.controller");
+const { verifyJWT } = require("../middlewares/auth.middleware");
 
-
-
-
-
+userRoutes.route("/profile").get(verifyJWT, getCurrentUser);
+userRoutes.route("/updateprofile").post(verifyJWT, updateAccountDetails);
 
 module.exports = userRoutes;
