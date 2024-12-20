2543e73b506ea4a132e4fbaf3a468b96147f6a45
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-21 00:59:56 +0530

    event.route update

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 const ApiError = require("../utils/ApiError");
-const asyncHandler  =require( "../utils/asyncHandler");
+const asyncHandler = require("../utils/asyncHandler");
 const process = require("node:process");
-const jwt = require( "jsonwebtoken");
-const User = require( "../models/user.model");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
 
 exports.verifyJWT = asyncHandler(async (req, _, next) => {
   try {
@@ -31,3 +31,24 @@ exports.verifyJWT = asyncHandler(async (req, _, next) => {
     throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized. Token missing!" });
+  }
+  try {
+    const decoded = jwt.verify(
+      token.split(" ")[1],
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    req.user = decoded; // Attach user payload to the request
+    next();
+  } catch {
+    return res.status(403).json({ message: "Invalid or expired token!" });
+  }
+};
+
+module.exports = {
+  verifyToken,
+};
