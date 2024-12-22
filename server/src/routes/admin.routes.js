77935d2913fb5af6afda55d9f58ddd6b1187cf48
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-23 01:51:52 +0530

    event controller update

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,2 +1,14 @@
 const express = require("express");
-const router = express.Router();
+const adminRoutes = express.Router();
+
+const {getUser, getUsersByEmail}= require("../controllers/admin.controller");
+
+// Get a all users
+
+adminRoutes.route("/users").get(getUser);
+adminRoutes.route("/users").get(getUsersByEmail);
+
+
+
+
+module.exports = adminRoutes;
