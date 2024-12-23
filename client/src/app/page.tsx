ab5e692a1046df2bf02c891c5af8fee95ebf2a74
Satyam Choubey <satyamchoubey789@gmail.com>
2024-12-24 03:51:11 +0530

    frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,3 @@
-
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -7,17 +5,6 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-center sm:text-5xl">
           Welcome to the <span className="text-indigo-500">Event</span> Management System!
         </h1>
-        <p className="text-lg text-center sm:text-xl">
-          Get started by creating an account or logging in.
-          </p>
-        <div className="flex flex-col gap-4">
-          <a href="/register" className="btn btn-primary">
-            Register
-          </a>
-          <a href="/login" className="btn btn-secondary">
-            Login
-          </a>
-        </div>
       </main>
     </div>
   );
