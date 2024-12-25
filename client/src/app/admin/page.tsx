e56baf83014bc27713bd287482781afa3599cded
Satyam Choubey <140761398+SatyamChoubey789@users.noreply.github.com>
2024-12-25 20:50:59 +0530

    Update page.tsx

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -3,7 +3,7 @@ export default function Admin(){
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <h1 className="text-4xl font-bold text-center sm:text-5xl">
-                    Admin page
+                    Admin
                 </h1>
                 <p className="text-lg text-center sm:text-xl">
                     This is the admin page. Here you can manage your events. You can
