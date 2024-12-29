72606e7e2b053cd3ccb2f81b98bcae1d89a2a8df
Satyam Choubey <140761398+SatyamChoubey789@users.noreply.github.com>
2024-12-29 22:57:56 +0530

    Update admin.controller.js

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../models/user.model")
 const ApiError = require("../utils/ApiError");
 const AsyncHandler = require("../utils/asyncHandler");
 
