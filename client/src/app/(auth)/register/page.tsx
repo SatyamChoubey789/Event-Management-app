87ac3893291547883eb26f2633ecd686e36c85c8
Satyam Choubey <140761398+SatyamChoubey789@users.noreply.github.com>
2025-01-01 00:48:36 +0530

    Update page.tsx

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ export default function Register(){
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <h1 className="text-4xl font-bold text-center sm:text-5xl">
-                    Register
+                    Register Page
                 </h1>
                 <p className="text-lg text-center sm:text-xl">
                     This is the registration page. Here you can create a new account. You can
