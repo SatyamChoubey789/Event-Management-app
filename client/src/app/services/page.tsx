37f0e4b2b67f8fd555c6507dac5f6019cb22125f
Satyam Choubey <140761398+SatyamChoubey789@users.noreply.github.com>
2025-01-01 00:50:39 +0530

    Update page.tsx

--- client/src/app/services/page.tsx
+++ client/src/app/services/page.tsx
@@ -3,7 +3,7 @@ export default function Services(){
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <h1 className="text-4xl font-bold text-center sm:text-5xl">
-                    Services
+                    Services Page
                 </h1>
                 <p className="text-lg text-center sm:text-xl">
                     We offer a variety of services to help you manage your events. Our
